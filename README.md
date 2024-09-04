81672a80048ac1dfa242e33c5d3cc9dc98f984d0
julio orozco <julio11orozco09gmail.com>
2024-09-04 01:10:24 -0600

    3: funcion mejorada

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Creación de  servicio para una bibliota
 - Primera parte del código, se enviará al otro desarrollador 
+
+- Segunda parte del codigo, mejoras en el programa, se enviará a la "rama main"
--- main.py
+++ main.py
@@ -16,37 +16,80 @@ libro3 = Libros("Antoine de Saint-Exupéry", "El Principito", "infantil \n")
 
 lista = [libro1, libro2, libro3]
 
-print("-----------------------------")
-print("-    Elija una opción       -")
-print("-   1: listar libros        -")
-print("-   2: recomendar un libro  -")
-print("-   3: salir...             -")
-print("-----------------------------")
+def mostrar_menu_principal():
+    print("-----------------------------")
+    print("-    Elija una opción       -")
+    print("-   1: listar libros        -")
+    print("-   2: recomendar un libro  -")
+    print("-   3: salir...             -")
+    print("-----------------------------")
+
+def Biblioteca():
+        mostrar_menu_principal()
+        while True:
+            opcion = int(input("menu de opciones: "))
+
+            if opcion == 1:
+                print("lista de libros disponibles")
+                for libro in lista:
+                    print(libro)
+
+            elif opcion == 2:
+                print("qué libro quiere recomendar?")
+                autor = input("nombre del autor: ")
+                titulo = input("titulo del libro: ")
+                genero = input("genero del libro: ")
+                libro_nuevo = Libros(autor, titulo, genero)
+                lista.append(libro_nuevo)
+
+            elif opcion == 3: 
+                print("gracias por usar el programa")
+                break
+            else: 
+                print("opción no válida, por favor elija una opción válida")
+
+
+            seguir = input("desea seguir usando el programa? (si/no): ").lower()
+            if seguir != "si":
+                print("hasta luego...")
+                break 
+
+usuarios = {}
 
 while True:
-    opcion = int(input("menu de opciones: "))
+  print("-------------------------")
+  print("-    Elija una opción   -")
+  print("-   1: Registrarse      -")
+  print("-   2: Iniciar sesión   -")
+  print("-   3: salir...         -")
+  print("-------------------------")
 
-    if opcion == 1:
-        print("lista de libros disponibles")
-        for libro in lista:
-            print(libro)
+  opcion = int(input("elija una opción: "))
 
-    elif opcion == 2:
-        print("qué libro quiere recomendar?")
-        autor = input("nombre del autor: ")
-        titulo = input("titulo del libro: ")
-        genero = input("genero del libro: ")
-        libro_nuevo = Libros(autor, titulo, genero)
-        lista.append(libro_nuevo)
+  if opcion == 1:
+      print("registrese sus datos")
+      nombre = input("cuál es su nombre:")
+      if nombre in usuarios:
+          print("ese nombre ya esta registrado, elija otro.")
+      else:
+        contraseña = input("ingrese una contraseña: ")
+        usuarios[nombre] = contraseña
+        print("Usuarios y contraseñas guardados:", usuarios)
+        print("registro exitoso \n")
 
-    elif opcion == 3: 
-        print("gracias por usar el programa")
-        break
-    else: 
-        print("opción no válida, por favor elija una opción válida")
+  elif opcion == 2:
+      print("iniciar sesión")
+      nombre = input("cuál es usuario:")
+      contraseña = input("digite su contraseña: ")
+      if nombre in usuarios and usuarios[nombre] == contraseña:
+        print("inicio de sesión exitoso")
+        Biblioteca()
+      else: 
+        print("Nombre de usuario o contraseña incorrectos \n")
 
-
-    seguir = input("desea seguir usando el programa? (si/no): ").lower()
-    if seguir != "si":
-        print("hasta luego...")
-        break 
+        
+  elif opcion == 3:
+    print("gracias por usar el programa")
+    break
+  else:
+    print("esa no es una opción válida")
